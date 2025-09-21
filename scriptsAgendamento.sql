49bb8c6a3a547f729e1edd166533d354b4316a4e
andrerikrdo777 <andrerikrdo777@gmail.com>
2025-09-21 16:04:32 -0300

    .

--- scriptsAgendamento.sql
+++ scriptsAgendamento.sql
@@ -1,12 +1,11 @@
 ------ SCRIPTS DE AGENDAMENTO ------
 
--- 9. SCRIPT PARA EXECUTAR VIA CRON (Linux) ou AGENDADOR DE TAREFAS (Windows)
-/*
--- Para executar via linha de comando:
+SCRIPT PARA EXECUTAR VIA CRON (Linux) ou AGENDADOR DE TAREFAS (Windows)
+
+Para executar via linha de comando:
 psql -U seu_usuario -d seu_banco -c "CALL dw.sp_etl_completo();"
 
 
--- Para Windows Task Scheduler, criar um arquivo .bat:
+Para Windows Task Scheduler, criar um arquivo .bat:
 @echo off
 psql -U seu_usuario -d seu_banco -c "CALL dw.sp_etl_completo();"
-*/
